3d92cb169f59eee70599c24d715c706278251644
Rafael Rodelo <he.is.rafaa@gmail.com>
2026-01-06 07:25:51 -0500

    Refactor student management options and imports

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from database import create_table, add_student, get_students
+from database import create_table
+from services import register_student, list_students
+
 
 def show_menu():
     print("\nStudent Management System")
@@ -14,19 +16,16 @@ def main():
         show_menu()
         option = input("Select an option: ")
 
-        if option == "1":
-            student_id = input("Student ID: ")
-            name = input("Name: ")
-            age = input("Age: ")
-            grade = input("Grade: ")
+if option == "1":
+    student_id = input("Student ID: ")
+    name = input("Name: ")
+    age = input("Age: ")
+    grade = input("Grade: ")
 
-            add_student(student_id, name, age, grade)
+    register_student(student_id, name, age, grade)
 
-        elif option == "2":
-            students = get_students()
-            print("\nRegistered Students:")
-            for student in students:
-                print(student)
+elif option == "2":
+    list_students()
 
         elif option == "3":
             print("Exiting system...")
